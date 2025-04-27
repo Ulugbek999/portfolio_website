d407d3a81106f971667977067c725d5e9e0ce2ac
Ulugbek999 <ulugbekabdurakhmonov.tennis@gmail.com>
2025-04-27 09:48:11 -0400

    Updated aboutMe + minor css updates

--- scripts.js
+++ scripts.js
@@ -1,18 +1,1 @@
-const nightModeButton = document.getElementById('nightModeButton');
 
-//nightModeButton.addEventListener('click', () => {
-  //  document.body.classList.toggle('night-mode');
-//});
-
-
-function change(){
-    //document.getElementById('d1').innerHTML="Hello World!!!"
-    //var para = document.getElementById('d1').getElementsByTagName('p')
-    //for(var i = 0; i<para.length; ++i){
-      //  para[i].innerHTML = i;
-    //}
-
-    //document.getElementById('img1').src == "nightMode.jpg"
-
-alert(document.getElementById('d1').childNodes.length)
-}
