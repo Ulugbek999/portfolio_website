18c3d10d80a965a9c3fd5246e68c02b4238b86de
Ulugbek999 <ulugbekabdurakhmonov.tennis@gmail.com>
2025-04-27 21:03:48 -0400

    changes in the projects and minor javaScript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-
+function toggleExpand(button){
+  const projectContent = button.closest('.project-content');
+  projectContent.classList.toggle('expanded');
+  if(projectContent.classList.contains('expanded')){
+    button.textContent = "Show less";
+  }else{
+    button.textContent = "Shoe more"
+  }
+  
+}
